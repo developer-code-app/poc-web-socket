1486e5ef41b8d23811cb423deda1b1ab44ea22d3
Pattarapon Tiewghan <pattarapon@code-app.com>
2024-04-17 16:19:41 +0700

    added property created_at

--- bin/server.dart
+++ bin/server.dart
@@ -2,12 +2,11 @@ import 'dart:convert';
 import 'dart:io';
 
 List<WebSocket> sockets = [];
+File file = File('messages.json');
 
 void main(List<String> args) async {
   final server = await HttpServer.bind('0.0.0.0', 8081);
   print('Listening on ws://${server.address.address}:${server.port}');
-
-  final data = await File('messages.json').readAsString().then(json.decode);
 
   await for (HttpRequest request in server) {
     if (WebSocketTransformer.isUpgradeRequest(request)) {
@@ -22,6 +21,8 @@ void main(List<String> args) async {
       switch (route) {
         case 'GET /chat_messages':
         case 'GET /chat_messages/':
+          final data = await file.readAsString().then(json.decode);
+
           request.response
             ..statusCode = HttpStatus.ok
             ..headers.set(HttpHeaders.contentTypeHeader,
@@ -43,8 +44,20 @@ void handleWebSocket(WebSocket socket) {
   sockets.add(socket);
 
   socket.listen(
-    (data) {
-      print('Received: $data');
+    (data) async {
+      final message = json.decode(data) as Map<String, dynamic>;
+
+      if (message['type'] == 'MESSAGE') {
+        final _message = message['message'] as Map<String, dynamic>;
+        final messages =
+            await file.readAsString().then(json.decode) as List<dynamic>;
+
+        messages.add(_message);
+
+        file.createSync();
+        file.writeAsStringSync(jsonEncode(messages));
+      }
+
       sockets.forEach((socket) => socket.add(data));
     },
     onDone: () {
